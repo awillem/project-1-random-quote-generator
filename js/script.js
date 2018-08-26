0211c5a479f5a773e5ec3b7cf0637cb1f83857ab
Emma Willmann <emma.anne.81@gmail.com>
2018-08-26 15:37:05 -0500

    added quote array. added random and print quote functions

--- js/script.js
+++ js/script.js
@@ -2,14 +2,167 @@
 
 // Create the array of quote objects and name it quotes
 
-
+var quotes = [
+    {
+      quote: 'All we have to decide is what to do with the time that is given to us.',
+      source: 'Gandalf',
+      citation: 'Lord of the Rings: The Fellowship of the Ring',
+      year: '2001',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'Life moves pretty fast.  If you don\'t stop and look around once in a while, you could miss it.',
+      source: 'Ferris Bueller',
+      citation: 'Ferris Bueller\'s Day Off',
+      year: '1986',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'Every man dies, but not every man really lives.',
+      source: 'William Wallace',
+      citation: 'Braveheart',
+      year:'1995' ,
+      tag: ['inspirational']
+    },
+    {
+      quote: 'Get busy livin\', or get busy dyin\'.',
+      source: 'The Shawshank Redemption',
+      citation: '',
+      year: '1994',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'Calling someone fat doesn\'t make you any skinnier.  Calling someone stupid doesn\'t make you any smarter.  All you can do in life is try and solve the problem in front of you.',
+      source: 'Cady Heron',
+      citation: 'Mean Girls',
+      year: '2004',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'It\'s not our abilities that show what we truly are.  It is our choices.',
+      source: 'Professor Dumbledore',
+      citation: 'Harry Potter and the Chamber of Secrets',
+      year: '2002',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'Strange, isn\'t it?  Each man\'s life touches so many other lives.  When he isn\'t around he leaves an awful hole, doesn\'t he?',
+      source: 'Clarence the Angel',
+      citation: 'It\'s a Wonderful Life',
+      year: '1946',
+      tag: ['inspirational', 'emotional']
+    },
+    {
+      quote: 'Do, or do not.  There is no try.',
+      source: 'Yoda',
+      citation: 'The Empire Strikes Back',
+      year: '1980',
+      tag: ['inspirational']
+    },
+    {
+      quote: 'It\'s just a flesh wound.',
+      source: 'The Black Knight',
+      citation: 'Monty Python and the Holy Grail',
+      year: '1975',
+      tag: ['humor']
+    },
+    {
+      quote: 'I am serious.  And don\'t call me Shirley.',
+      source: 'Dr. Rumack',
+      citation: 'Airplane',
+      year: '1980',
+      tag: ['humor']
+    },
+    {
+      quote: 'Martini.  Gin, not vodka.  Obviously.  Stirred for 10 seconds while glancing at an unopened bottle of vermouth.',
+      source: 'Eggsy',
+      citation: 'Kingsman: The Secret Service',
+      year: '2014',
+      tag: ['humor']
+    },
+    {
+      quote: 'Leave the gun. Take the cannoli.',
+      source: 'Peter Clemenza',
+      citation: 'The Godfather',
+      year: '1972',
+      tag: ['humor']
+    },
+    {
+      quote: 'It\'s like I have ESPN or something.',
+      source: 'Karen Smith',
+      citation: 'Mean Girls',
+      year: '2004',
+      tag: ['humor']
+    },
+    {
+      quote: 'Excuse me.  I believe you have my stapler.',
+      source: 'Milton Waddams',
+      citation: 'Office Space',
+      year: '1999',
+      tag: ['humor']
+    },
+    {
+      quote: 'It\'s not a man purse.  It\'s a satchel.  Indiana Jones wears one.',
+      source: 'Alan Garner',
+      citation: 'The Hangover',
+      year: '2009',
+      tag: ['humor']
+    },
+    {
+      quote: 'You had me at hello.',
+      source: 'Dorothy Boyd',
+      citation: 'Jerry Maguire',
+      year: '1996',
+      tag: ['emotional']
+    },
+    {
+      quote: 'Always.',
+      source: 'Severus Snape',
+      citation: 'Harry Potter and the Deathly Hallows Part 2',
+      year: '2011',
+      tag: ['emotional']
+    },
+    {
+      quote: 'You is kind. You is smart. You is important.',
+      source: 'Aibileen Clark',
+      citation: 'The Help',
+      year: '2011',
+      tag: ['emotional','inspirational']
+    },
+    {
+      quote: 'I would have followed you, my brother, my captain, my king.',
+      source: 'Boromir',
+      citation: 'Lord of the Rings: The Fellowship of the Ring',
+      year: '2001',
+      tag: ['emotional','inspirational']
+    },
+    {
+      quote: 'To live.  To live would be an awfully big adventure.',
+      source: 'Peter \'Pan\' Banning',
+      citation: 'Hook',
+      year: '1991',
+      tag: ['emotional', 'inspirational']
+    },
+]
 
 // Create the getRandomQuuote function and name it getRandomQuote
-
+function getRandomQuote (array) {
+  var random = Math.floor(Math.random() * array.length);
+  return (array[random]);
+}
 
 
 // Create the printQuote funtion and name it printQuote
-
+function printQuote () {
+  var randomQuote = getRandomQuote(quotes);
+  var html = '';
+  html += '<p class="quote">' + randomQuote.quote;
+  html += '</p> <p class="source">' + randomQuote.source;
+  html += '<span class="citation">' + randomQuote.citation;
+  html += '</span><span class="year">' + randomQuote.year;
+  document.getElementById('quote-box').innerHTML = html;
+}
+printQuote();
 
 
 // This event listener will respond to "Show another quote" button clicks
